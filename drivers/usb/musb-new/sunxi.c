ffdcd6f52bbd57b0fd78e36e0a2c0d510eb050b6
Max Lampert <ml@mlampert.de>
2026-01-12 02:49:29 +0100

    Revert MUSB DM USB gadget patches - not needed
    Reverts:
    - fix: use usb_gadget_generic_ops instead of global dm_usb_gadget_handle_interrupts
    - usb: musb-new: sunxi: make compatible with UDC/DM gadget model
    - usb: musb-new: sunxi: remove unwanted printfs

--- drivers/usb/musb-new/sunxi.c
+++ drivers/usb/musb-new/sunxi.c
@@ -428,21 +428,6 @@ static struct musb_hdrc_config musb_config_h3 = {
 	.ram_bits	= SUNXI_MUSB_RAM_BITS,
 };
 
-#if CONFIG_IS_ENABLED(DM_USB_GADGET)
-static int sunxi_musb_gadget_handle_interrupts(struct udevice *dev)
-{
-	struct sunxi_glue *glue = dev_get_priv(dev);
-	struct musb_host_data *host = &glue->mdata;
-
-	host->host->isr(0, host->host);
-	return 0;
-}
-
-static const struct usb_gadget_generic_ops sunxi_musb_gadget_ops = {
-	.handle_interrupts = sunxi_musb_gadget_handle_interrupts,
-};
-#endif
-
 static int musb_usb_probe(struct udevice *dev)
 {
 	struct sunxi_glue *glue = dev_get_priv(dev);
@@ -450,6 +435,10 @@ static int musb_usb_probe(struct udevice *dev)
 	struct musb_hdrc_platform_data pdata;
 	void *base = dev_read_addr_ptr(dev);
 	int ret;
+
+#ifdef CONFIG_USB_MUSB_HOST
+	struct usb_bus_priv *priv = dev_get_uclass_priv(dev);
+#endif
 
 	if (!base)
 		return -EINVAL;
@@ -481,31 +470,27 @@ static int musb_usb_probe(struct udevice *dev)
 	pdata.platform_ops = &sunxi_musb_ops;
 	pdata.config = glue->cfg->config;
 
-	if (IS_ENABLED(CONFIG_USB_MUSB_HOST)) {
-		struct usb_bus_priv *priv = dev_get_uclass_priv(dev);
-		priv->desc_before_addr = true;
+#ifdef CONFIG_USB_MUSB_HOST
+	priv->desc_before_addr = true;
 
-		pdata.mode = MUSB_HOST;
-		host->host = musb_init_controller(&pdata, &glue->dev, base);
-		if (!host->host)
-			return -EIO;
+	pdata.mode = MUSB_HOST;
+	host->host = musb_init_controller(&pdata, &glue->dev, base);
+	if (!host->host)
+		return -EIO;
 
-		return musb_lowlevel_init(host);
-	} else if (CONFIG_IS_ENABLED(DM_USB_GADGET)) {
-		pdata.mode = MUSB_PERIPHERAL;
-		host->host = musb_init_controller(&pdata, &glue->dev, base);
-		if (!host->host)
-			return -EIO;
+	ret = musb_lowlevel_init(host);
+	if (!ret)
+		printf("Allwinner mUSB OTG (Host)\n");
+#else
+	pdata.mode = MUSB_PERIPHERAL;
+	host->host = musb_register(&pdata, &glue->dev, base);
+	if (IS_ERR_OR_NULL(host->host))
+		return -EIO;
 
-		return usb_add_gadget_udc(&glue->dev, &host->host->g);
-	} else {
-		pdata.mode = MUSB_PERIPHERAL;
-		host->host = musb_register(&pdata, &glue->dev, base);
-		if (IS_ERR_OR_NULL(host->host))
-			return -EIO;
+	printf("Allwinner mUSB OTG (Peripheral)\n");
+#endif
 
-		return 0;
-	}
+	return ret;
 }
 
 static int musb_usb_remove(struct udevice *dev)
@@ -572,8 +557,6 @@ U_BOOT_DRIVER(usb_musb) = {
 	.remove		= musb_usb_remove,
 #ifdef CONFIG_USB_MUSB_HOST
 	.ops		= &musb_usb_ops,
-#elif CONFIG_IS_ENABLED(DM_USB_GADGET)
-	.ops		= &sunxi_musb_gadget_ops,
 #endif
 	.plat_auto	= sizeof(struct usb_plat),
 	.priv_auto	= sizeof(struct sunxi_glue),
