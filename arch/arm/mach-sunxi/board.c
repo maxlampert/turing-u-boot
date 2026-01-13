e7dc6a44b059f08c4befda384848cc2e67a6f675
Max Lampert <ml@mlampert.de>
2026-01-13 23:38:33 +0100

    Fix: void unblock_twi2_bus() function declaration isn’t a prototype Fix: implicit declaration of function ‘udelay’

--- arch/arm/mach-sunxi/board.c
+++ arch/arm/mach-sunxi/board.c
@@ -29,6 +29,7 @@
 #include <asm/arch/mmc.h>
 
 #include <linux/compiler.h>
+#include <linux/delay.h>
 
 int tp_board_init(void);
 
@@ -469,7 +470,8 @@ u32 spl_mmc_boot_mode(struct mmc *mmc, const u32 boot_device)
 
 // In the event the bus hanged because of prior operation, clock out any
 // residual operations.
-void unblock_twi2_bus() {
+void unblock_twi2_bus(void)
+{
     unsigned SCL = SUNXI_GPE(12);
     unsigned SDA = SUNXI_GPE(13);
     gpio_direction_output(SCL, 0);
